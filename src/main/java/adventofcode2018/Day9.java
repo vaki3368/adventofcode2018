b57aed011d92974171a1d6e085eac4d8ecd09206
Valerij Kislicyn <valerij.kislicyn@esolutions.de>
2018-12-13 10:42:36 +0100

    day 9-2

--- src/main/java/adventofcode2018/Day9.java
+++ src/main/java/adventofcode2018/Day9.java
@@ -1,53 +1,41 @@
 package adventofcode2018;
 
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
-import com.google.common.collect.Iterables;
-
+import java.util.Deque;
 
 public class Day9 {
 
 	public static void main(String[] args) {
-		int players = 464;
-		int lastMarble = 71730 * 100;
-		List<Integer> input = new LinkedList<>();
-		int[] playerPoints = new int[players];
+		int players = 464; //464;//464;
+		int lastMarble = 71730 * 100;// 71730 * 100;//71730 * 100;
+		Deque<Integer> input = new ArrayDeque<>();
+		long[] playerPoints = new long[players];
 		Arrays.fill(playerPoints, 0);
-		int highScore = 0;
-
-		input.add(0);
-
-		int pointer = 0;
+		long highScore = 0;
+		input.addFirst(0);
 		for (int i = 1; i <= lastMarble; i++) {
-
-			if(i %10000 == 0)
-			  System.out.println(i);
 			int currentPlayer = i % players;
 
 			if (i % 23 == 0) {
 				playerPoints[currentPlayer] += i;
 
-				int deletionIndex = (pointer - 7 + input.size()) % input.size();
-
-				playerPoints[currentPlayer] += input.remove(deletionIndex + 1);
+				for (int j = 0; j < 6; j++) {
+					input.addLast(input.remove());
+				}
+				
+				playerPoints[currentPlayer] += input.pop();
 
 				if (playerPoints[currentPlayer] > highScore) {
 					highScore = playerPoints[currentPlayer];
 				}
-
-				pointer = deletionIndex;
-
 			} else {
-
-				pointer = (pointer + 2) % input.size();
-				input.add(1 + pointer, i);
+				input.addFirst(input.removeLast());
+				input.addFirst(input.removeLast());
+				input.addLast(i);
 			}
-
-			// System.out.println(Iterables.toString(input));
-			// System.out.println(Arrays.toString(playerPoints));
+			//System.out.println(Iterables.toString(input));
+			//System.out.println(Arrays.toString(playerPoints));
 		}
 		System.out.println(highScore);
 	}
